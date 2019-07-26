ada7aebef1820bdd7db2b16eed8d43080441206b
Fabrizio Rovaris <f.rovaris1@campus.unimib.it>
2019-07-26 10:19:21 +0200

    Removed useless imports

--- splitanswers.py
+++ splitanswers.py
@@ -1,19 +1,17 @@
 # -- coding: utf-8 --
-import os, time
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
-def split answers.py(img):
+def splitanswers(img):
   flip = 1 
   positions = []
   for i in range(len(img[:,1135])):
     if img[-1-i,1335] > 220:
       while flip !=0 :
-        positions.append(len(img[:,1135]-i)
+        positions.append(len(img[:,1135])-i)
         flip = 0
     else: 
       while flip !=1 :
-        positions.append(len(img[:,1135]-i)
+        positions.append(len(img[:,1135])-i)
         flip = 1 
-return positions[0:7]
+  return positions[0:7]
