1187ebb485fc429a581ad9a5bd753d20162ed1f3
jacopo <trunksn1@gmail.com>
2019-07-30 23:04:22 +0200

    file utils per funzioni utili per i test

---
+++ utils.py
@@ -0,0 +1,60 @@
+import cv2, os, glob
+import numpy as np
+from cf import SCREEN_DIR
+from Elaboratore import Elaboratore
+from Identificatore import Identificatore
+
+PATH_SEPARATE = os.path.join(SCREEN_DIR, 'da_concatenare')
+PATH_CONCATENATE = os.path.join(SCREEN_DIR, 'concatenate')
+PATH_DOM_RSP = os.path.join(SCREEN_DIR, 'Domande_Risposte')
+print(PATH_SEPARATE)
+print(PATH_CONCATENATE)
+
+def aggancia_dom_e_risp():
+    """Serve a incollare insieme gli screenshot in cui ho domande e risposte staccate"""
+    if not os.path.isdir(PATH_CONCATENATE):
+        os.makedirs(PATH_CONCATENATE)
+
+    # Ottengo la lista di files nella cartella PATH_SEPARATE
+    files = glob.glob(os.path.join(PATH_SEPARATE, '*'), recursive=True)
+    # Ordino la lista in base alla data di creazione dei file (così da avere in successione: domanda + rispsota)
+    files.sort(key=os.path.getmtime)
+
+    # dalla lista separo le domande dalle risposte, gli elementi pari sono domande, i dispari le risposte
+    dom = files[::2]
+    risp = files[1::2]
+
+
+    for n in range (len(dom)):
+        dom1 = cv2.imread(dom[n])
+        ris1 = cv2.imread(risp[n])
+        # Prese le immagini le concateno l'una sopra l'altra (axis 0)
+        vis = np.concatenate((dom1, ris1), axis=0)
+
+        cv2.imwrite('out{}.png'.format(n), vis)
+
+def diario():
+    immagini_da_studiare = glob.glob(os.path.join(PATH_DOM_RSP, '*'), recursive=True)
+    print(immagini_da_studiare)
+    with open(os.path.join(PATH_DOM_RSP, 'diario.txt'), 'w') as log:
+        for n, immagine in enumerate(immagini_da_studiare):
+            #if n == 10:
+            #    break
+            try:
+                el = Elaboratore(immagine)
+                id = Identificatore(el.pezzi)
+                print(id.domanda)
+                print(id.risposte)
+                mess = '****** Domanda numero: {} ******'.format(n)
+                log.write(mess + '\n')
+                log.write(id.domanda + '\n\t')
+                log.write('\n\t'.join(id.risposte))
+                log.write('\n' + '\n')
+            except:
+                continue
+
+
+
+if __name__ == '__main__':
+    #aggancia_dom_e_risp()
+    diario()
