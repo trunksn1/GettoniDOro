e1622ba31e1763955dcb48c0b3d2e96622a37766
Jacopo Taurino <trunksn1@gmail.com>
2019-08-15 23:05:46 +0200

    non ricordo di aver introdotto conflitti

--- cf.py
+++ cf.py
@@ -35,3 +35,7 @@ y_finale_risposte = 320
 #Dimensioni e posizione della finestra del browser che si apre
 coordinate_browser = (1125, 0) #si apre in mezzo per lasciare al centro il quiz, il valore sul pc Milanese è 800,0
 dimensioni_browser = (850, 1080)
+
+
+
+CONFLITTO INTRODOTTO SOLO PER 
