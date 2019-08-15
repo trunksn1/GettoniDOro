955f47b2a2cbd5c73ab206a43b5465d48dd79e28
trunksn1 <trunksn1@gmail.com>
2019-08-15 20:15:13 +0200

    modificato leggermente il comportamento della funzione che mostra la GUI

--- Guiatore.py
+++ Guiatore.py
@@ -13,7 +13,8 @@ class Guiatore():
         threading.Thread(target=self.aggiornatore_gui, args=(gui_queue, punteggi[1], '_dr_RX_',), daemon=True).start()
         threading.Thread(target=self.aggiornatore_gui, args=(gui_queue, punteggi[2], '_TOT_RX_',), daemon=True).start()
         # -- Start the GUI passing in the Queue --
-        self.gui_punteggi(gui_queue)
+        dict_dati_per_gui = self.queue_to_dict(gui_queue)
+        self.gui_punteggi(dict_dati_per_gui)
 
     def aggiornatore_gui(self, gui_queue, punteggio, key):
         for n, el in enumerate(self.lista_risposte):  # loop forever, keeping count in i as it loops
@@ -22,10 +23,11 @@ class Guiatore():
             key = key[:-2] + str( n +1) + key[-1:]
             gui_queue.put((key, punteggio[el]))
 
-    def gui_punteggi(self, gui_queue):
+    def gui_punteggi(self, dati):# gui_queue):
         # TODO: il programma si blocca quando mostra la GUI, bisogna sfruttrare i thread
         sg.ChangeLookAndFeel('GreenTan')
 
+        """
         layout = [
             # [sg.Text('RISPOSTE', size=(15, 1)), sg.Text('SoloD', size=(15, 1)), sg.Text('+Risp', size=(15, 1)), sg.Text('TOT', size=(15, 1))],
             [sg.Text('RISPOSTE', size=(15, 1)), sg.Text('SoloD'), sg.Text('+Risp'), sg.Text('TOT')],
@@ -34,13 +36,35 @@ class Guiatore():
             [sg.Text(self.lista_risposte[2], size=(15, 1)), sg.Text('', key='_d_R3_', justification='right', size=(3, 1)), sg.Text('', key='_dr_R3_', justification='right', size=(3, 1)), sg.Text('', key='_TOT_R3_', justification='right', size=(3, 1))],
             [sg.Exit()]
         ]
+        """
 
 
-        window = sg.Window('Risposte', layout, default_element_size=(40, 1), grab_anywhere=True, return_keyboard_events=True)
+        layout = [
+            [sg.Text('RISPOSTE', size=(15, 1)), sg.Text('SoloD'), sg.Text('+Risp'), sg.Text('TOT')],
+            [sg.Text(self.lista_risposte[0], size=(15, 1)), sg.Text(dati['_d_R1_'], key='_d_R1_', justification='right', size=(3, 1)), sg.Text(dati['_dr_R1_'], key='_dr_R1_', justification='right', size=(3, 1)), sg.Text(dati['_TOT_R1_'], key='_TOT_R1_', justification='right', size=(3, 1))],
+            [sg.Text(self.lista_risposte[1], size=(15, 1)), sg.Text(dati['_d_R2_'], key='_d_R2_', justification='right', size=(3, 1)), sg.Text(dati['_dr_R2_'], key='_dr_R2_', justification='right', size=(3, 1)), sg.Text(dati['_TOT_R2_'], key='_TOT_R2_', justification='right', size=(3, 1))],
+            [sg.Text(self.lista_risposte[2], size=(15, 1)), sg.Text(dati['_d_R3_'], key='_d_R3_', justification='right', size=(3, 1)), sg.Text(dati['_dr_R3_'], key='_dr_R3_', justification='right', size=(3, 1)), sg.Text(dati['_TOT_R3_'], key='_TOT_R3_', justification='right', size=(3, 1))],
+            [sg.Exit()]
+        ]
 
+        window = sg.Window('Risposte', default_element_size=(40, 1), grab_anywhere=True, return_keyboard_events=True).Layout(layout)
+
+        start = time.time()
+
+        event, values = window.Read(timeout=8000) # dopo otto secondi la funzione termina da sola
+        if event == 'Exit' or event is None or event == 'F9:120': #altrimenti termina se premo F9
+            window.Close()
+        dur = time.time() - start
+        print(dur)
+        window.Close()
+
+        """
         while True:
+
             event, values = window.Read(timeout=10)  # wait for up to 100 ms for a GUI event
-            if event is None or event == 'F9:120' or event == 'Exit':
+            dur = time.time() - start
+            if event is None or event == 'F9:120' or event == 'Exit' or (dur > 10):
+                print('Tempo scaduto: ', dur)
                 break
             # --------------- Loop through all messages coming in from threads ---------------
             while True:  # loop executes until runs out of messages in Queue
@@ -56,4 +80,17 @@ class Guiatore():
 
         # if user exits the window, then close the window and exit the GUI func
         window.Close()
+        """
 
+    def queue_to_dict(self, gui_queue):
+        key_punteggio = {}
+        while True:
+            try:
+                message = gui_queue.get_nowait()
+            except queue.Empty:
+                break
+            if message:
+                key_punteggio[message[0]] = message[1]
+        print('lista creata dalla Queu: \n')
+        print(key_punteggio)
+        return key_punteggio
