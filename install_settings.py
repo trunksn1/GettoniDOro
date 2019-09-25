80395a718345acb2200b1dd2b06507da8a6a91d7
jacopo <trunksn1@gmail.com>
2019-09-25 22:07:15 +0200

    rimosso in file install_settings dal version control

--- install_settings.py
+++
@@ -1,8 +0,0 @@
-import os
-if (os.name=='posix'):
-    PATH_INSTALLAZIONE_TESSERACT = ('/usr/local/bin/tesseract')
-    CHROME = os.path.join('Applications', 'Google Chrome.app', 'Contents', 'MacOS', 'Google  Chrome')
-else:
-    PATH_INSTALLAZIONE_TESSERACT = r'E:\Tesseract-OCR\tesseract.exe'
-    CHROME = os.path.join('C:\\', 'Program Files (x86)', 'Google', 'Chrome', 'Application', 'chrome.exe')
-    WEBDRIVER_PATH = os.path.join('C:\\', 'Program Files (x86)', 'ChromeDriverForSelenium', 'chromedriver.exe')
